80f9a52ec9173fedac27ed2010385a110551e614
Ventsislav Peychev <del.busque@gmail.com>
2023-05-28 13:29:54 +0300

    Remove temporaries

--- mpa-helpers.js
+++
@@ -1,6 +0,0 @@
-app.set('views', path.resolve('./src/views'))
-app.set('views', path.resolve(__dirname, '../views'))
-
-app.use(express.static(path.resolve(__dirname, './public')))
-
-app.use(express.urlencoded({extended:true}))
