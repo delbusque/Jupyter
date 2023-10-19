7765075b3a5baffeddf234b3bd515b69c882aaf3
delbusque <del.busque@gmail.com>
2023-10-19 10:32:31 +0300

    Add info and add button

--- react-native/react-native-cards/App.js
+++ react-native/react-native-cards/App.js
@@ -1,9 +1,10 @@
-import { StatusBar, SafeAreaView, ScrollView, StyleSheet, Text, View, Image } from 'react-native';
+import { StatusBar, SafeAreaView, ScrollView, StyleSheet, Text, View, Image, Pressable } from 'react-native';
 import pizza from './assets/pizza.png'
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 import { faCirclePlus } from '@fortawesome/free-solid-svg-icons/'
 
 export default function App() {
+
 
   return (
     <ScrollView style={styles.container}>
@@ -22,7 +23,32 @@ export default function App() {
             <FontAwesomeIcon icon={faCirclePlus} style={styles.icon} size={20} />
             <Text style={styles.iconIngr}>Add ingredients</Text>
           </View>
+
+          <View style={styles.bottomContainer}>
+            <View style={styles.priceContainer}>
+              <Text style={[styles.price, styles.priceReduced]}>$18 </Text>
+              <Text style={[styles.price, styles.priceOld]}> $22 </Text>
+              <Text style={styles.priceNormal}> / 550 g</Text>
+            </View>
+
+            <View style={styles.countContainer}>
+              <Pressable>
+                <Text style={styles.countBtnMinus}>-</Text>
+              </Pressable>
+              <Text style={styles.counter}>1</Text>
+              <Pressable>
+                <Text style={styles.countBtn}>+</Text>
+              </Pressable>
+            </View>
+          </View>
+
+          <Pressable style={styles.addBtn}>
+            <Text style={styles.addBtnText}>ADD TO ORDER</Text>
+          </Pressable>
+
         </View>
+
+
 
 
 
@@ -34,6 +60,66 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+  addBtnText: {
+    color: 'white',
+    fontFamily: 'notoserif'
+  },
+  addBtn: {
+    marginTop: 25,
+    alignSelf: 'center',
+    backgroundColor: '#fd7a5c',
+    paddingVertical: 15,
+    paddingHorizontal: 100,
+    borderRadius: 15
+  },
+  counter: {
+    paddingHorizontal: 12,
+    paddingVertical: 2,
+    fontSize: 15,
+    marginRight: 5
+  },
+  countBtn: {
+    fontSize: 16,
+    backgroundColor: '#dedfe4',
+    paddingHorizontal: 10,
+    paddingVertical: 2,
+    borderRadius: 5
+  },
+  countBtnMinus: {
+    fontSize: 16,
+    backgroundColor: '#dedfe4',
+    paddingHorizontal: 12,
+    paddingVertical: 2,
+    borderRadius: 5,
+    marginRight: 5
+  },
+  bottomContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 20
+  },
+  priceContainer: {
+    flexDirection: 'row'
+  },
+  price: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: 'grey',
+  },
+  priceReduced: {
+    color: '#64c97d',
+  },
+  priceOld: {
+    textDecorationLine: 'line-through',
+    color: 'grey'
+  },
+  priceNormal: {
+    fontSize: 16,
+    color: 'grey'
+  },
+  countContainer: {
+    flexDirection: 'row',
+  },
   container: {
     flex: 1,
     backgroundColor: '#cddcfd',
@@ -67,7 +153,6 @@ const styles = StyleSheet.create({
     borderRadius: 15,
     marginHorizontal: 7,
     width: '95%',
-    height: 200,
     top: -10,
     padding: 20
   },
@@ -90,6 +175,7 @@ const styles = StyleSheet.create({
   },
   iconIngr: {
     color: 'gray',
+    fontWeight: 'bold'
   }
 
 });
